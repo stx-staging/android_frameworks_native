d06cd4d27cce93b981de39c9e5700190d6726bb4
Pullakavi Srinivas <spullaka@codeaurora.org>
2019-06-20 21:12:08 +0530

    sf: Resync model on fps change.
    CRs-Fixed: 2418977
    Change-Id: I9a267e5f39386a6cfef638c29a3ee5fbfaa7d557

--- services/surfaceflinger/SurfaceFlinger.h
+++ services/surfaceflinger/SurfaceFlinger.h
@@ -759,6 +759,7 @@ private:
 
     sp<DisplayDevice> getVsyncSource();
     void updateVsyncSource();
+    void forceResyncModel();
     void preComposition();
     void postComposition();
     void getCompositorTiming(CompositorTiming* compositorTiming);
@@ -1040,6 +1041,9 @@ private:
     std::atomic<uint32_t> mFrameMissedCount = 0;
     std::atomic<uint32_t> mHwcFrameMissedCount = 0;
     std::atomic<uint32_t> mGpuFrameMissedCount = 0;
+
+    std::mutex mVsyncPeriodMutex;
+    std::vector<nsecs_t> mVsyncPeriod;
 
     TransactionCompletedThread mTransactionCompletedThread;
 
